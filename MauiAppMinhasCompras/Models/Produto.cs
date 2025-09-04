f0edc30b19266fff78e200fb214c3f3fcec5bb7d
João <jhsp350@gmail.com>
2025-09-04 08:28:50 -0300

    add a Quantidade e Preco validação dos valores

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -3,11 +3,48 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _valor;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a Descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _valor;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha a Quantidade");
+                }
+
+                _valor = value;
+            }
+        }
+        public double Preco
+        {
+            get => _valor;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha o Preço");
+                }
+            }
+        }
 
         public double Total { get => Quantidade * Preco; }
     }
